840483d955ceabee533a5c7db767269e0d93dbb9
Stanley Ulili <stanleyulili@gmail.com>
2021-09-23 20:47:18 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="home page">
       <h1>Welcome to my Gatsby site!</h1>
+      <StaticImage
+        src="../images/clifford.jpeg"
+        alt="This is an image of an animal, am just lazy coming up with a description"
+      />
     </Layout>
   );
 };
